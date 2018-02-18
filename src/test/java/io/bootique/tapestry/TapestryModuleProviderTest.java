1ed156fec3ad82ffb031bc9828de70af841c62a7
Andrus Adamchik <andrus@objectstyle.com>
2018-02-18 10:39:36 +0300

    Declare module dependencies bootique/bootique#203
    * new test API

--- src/test/java/io/bootique/tapestry/TapestryModuleProviderTest.java
+++ src/test/java/io/bootique/tapestry/TapestryModuleProviderTest.java
@@ -3,11 +3,10 @@ package io.bootique.tapestry;
 import io.bootique.BQRuntime;
 import io.bootique.jetty.JettyModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQRuntimeChecker;
 import io.bootique.test.junit.BQTestFactory;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static com.google.common.collect.ImmutableList.of;
 
 public class TapestryModuleProviderTest {
 
@@ -27,9 +26,9 @@ public class TapestryModuleProviderTest {
     @Test
     public void testModuleDeclaresDependencies() {
         final BQRuntime bqRuntime = testFactory.app().module(new TapestryModuleProvider()).createRuntime();
-        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(
+        BQRuntimeChecker.testModulesLoaded(bqRuntime,
                 TapestryModule.class,
                 JettyModule.class
-        ));
+        );
     }
 }
